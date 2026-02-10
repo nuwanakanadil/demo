dd16c4bdaff1c4b36f35a40440fef5c46f842e4e
Nuwanaka Nadil <86093181+nuwanakanadil@users.noreply.github.com>
2026-02-11 02:03:12 +0530

    problem solved kinda

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,15 +1,9 @@
-// src/api/axios.ts
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_URL ||
-  "http://localhost:5000/api"; // local fallback
-
 const api = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
