096ea4642b1545e46c458c6f1988edc28078b0a7
gamer0236 <wwwabcdef577@gmail.com>
2026-02-10 21:28:06 +0530

    Aded User profile management

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -45,3 +45,27 @@ export async function getMe() {
   const res = await api.get("/auth/me");
   return res.data as MeResponse;
 }
+
+
+export async function updateMe(payload: {
+  name?: string;
+  newPassword?: string;
+  confirmPassword?: string;
+}) {
+  const res = await api.patch("/auth/me", payload);
+  return res.data as {
+    success: boolean;
+    message: string;
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      role: "user" | "admin";
+      isEmailVerified: boolean;
+    };
+  };
+}
+export async function deleteMe() {
+  const res = await api.delete("/auth/me");
+  return res.data as { success: boolean; message: string };
+}
